baf4e8f9f1af9b7bf837a7c824819923deffb24d
aaa <aaa>
2025-09-20 12:29:15 +0900

    aaaaa

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtPage />
+    <ContentRenderer :value="post" />
+</template>
+
+<script setup>
+const slug = useRoute().params.slug
+const { data: post } = await useAsyncData(`blog-${slug}`, () => {
+  return queryCollection('blog').path(`/blog/${slug}`).first()
+})
+</script>
