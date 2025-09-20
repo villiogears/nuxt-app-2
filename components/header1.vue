8883e6b56c87077aaa21b767d4ca7b18ff86da76
aaa <aaa>
2025-09-20 21:49:48 +0900

    aaaaa

--- components/header1.vue
+++ components/header1.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="text-white">
-        <div class="color_blue max-w-3xl mx-auto px-4 py-5 center2 center3 text-4xl">
+    <header class="bg-green-600 text-white">
+        <div class="max-w-3xl mx-auto px-4 py-5 center2 center3 text-4xl">
             elphadeal
         </div>
     </header>
