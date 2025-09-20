26a0e1b7dc7c679b9e594a0b823e945e3e785326
aaa <aaa>
2025-09-20 20:58:42 +0900

    aaaaa

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
 <template>
     <NuxtPage />
-    
+    <elphadeal />
 </template>
--- components/elphadeal.vue
+++ components/elphadeal.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        
+    <div class="center1">
+        <button type="button" class="px-4 py-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed">
+            ボタン
+        </button>
     </div>
 </template>
 
