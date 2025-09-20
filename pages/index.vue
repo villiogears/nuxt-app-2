4f0226567d2aa93a62114a0d8a8a41b5c376ac7b
aaa <aaa>
2025-09-20 20:39:30 +0900

    aaaaa

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-    <NuxtPage />
-    <defineNuxtComponent />
-    <UButton color="secondary" variant="subtle" label="Button" />
+  <div>
+    <NuxtUButton color="secondary" variant="subtle" label="Button" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const value = ref(true)
 </script>
