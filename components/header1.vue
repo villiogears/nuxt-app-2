c3eda5ee5a1a9c959521906fcf847f3cdc4775bc
aaa <aaa>
2025-09-20 21:12:05 +0900

    aaaaa

--- components/header1.vue
+++ components/header1.vue
@@ -1,6 +1,7 @@
 <template>
-    <header class="bg-blue-600 text-white w-full">
-        <div class="max-w-5xl mx-auto px-4 py-30 flex items-center justify-between">
+    <header class="bg-green-600 text-white w-full">
+        <div class="max-w-5xl mx-auto px-4 py-7 flex items-center justify-between">
+            
         </div>
     </header>
 </template>
