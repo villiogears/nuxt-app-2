5356ec928fe15d0e9d0b79732fbd0c7a6c43098f
aaa <aaa>
2025-09-21 01:57:15 +0900

    aaaaa

--- components/elphadeal.vue
+++ components/elphadeal.vue
@@ -1,6 +1,7 @@
 <template>
     <footer class="center4 footer1">
-        <h1 class="text-center">
+        <h1 class="text-center text-3xl">
+            elphadeal
         </h1>
     </footer>
 </template>
