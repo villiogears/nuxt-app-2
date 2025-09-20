a96050fe64bf0d01296f23f52dec923ecd5b677a
aaa <aaa>
2025-09-20 12:34:23 +0900

    aaaaa

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -8,4 +8,5 @@ const slug = useRoute().params.slug
 const { data: post } = await useAsyncData(`blog-${slug}`, () => {
   return queryCollection('blog').path(`/${slug}`).first()
 })
+const allPosts = await queryCollection('blog').order('title', 'date').all()
 </script>
