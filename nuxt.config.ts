3d43a72b3936348b33df572b1a81c5f0e0522efe
aaa <aaa>
2025-09-20 12:21:27 +0900

    aaaaa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+    ui: {
+    prefix: 'Nuxt'
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
