83b899d2187e72df2737d41ed8736d8e6a810f14
aaa <aaa>
2025-09-20 22:38:04 +0900

    aaaaa

--- components/elphadeal.vue
+++ components/elphadeal.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="center1">
-        <button type="button" class="px-4 py-2 bg-green-600 hover:bg-green-700 active:bg-green-800 text-white font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed">
-            ボタン
-        </button>
-    </div>
 </template>
 
 <script lang="ts">
 
 </script>
+
+<style>
+body {
+    background: linear-gradient(to bottom right, rgb(0, 0, 0), rgb(255, 169, 183));
+}
+</style>
